bcd77146d3929a66a9a8b39b6e00e06eb73359e6
Jebermudez <jebermudez@unal.edu.co>
2025-12-08 17:08:16 -0500

    Added dockerfile, buttons now exist outside of the main card

--- src/Pages/RightPanel.jsx
+++ src/Pages/RightPanel.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
-import { HeartIcon } from "@heroicons/react/24/solid";
-import { XMarkIcon } from "@heroicons/react/24/outline";
 
-export default function RightPanel() {
-  const [animation, setAnimation] = useState("");
+
+export default function RightPanel({animation}) {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [showDescription, setShowDescription] = useState(false);
 
@@ -15,15 +13,7 @@ export default function RightPanel() {
     description: "Amante de la música y las aventuras. Me encanta viajar y conocer gente nueva. ¡Espero encontrar a alguien con quien compartir risas!"
   };
 
-  const animateLeft = () => {
-    setAnimation("animate-slide-left");
-    setTimeout(() => setAnimation(""), 500);
-  };
 
-  const animateRight = () => {
-    setAnimation("animate-slide-right");
-    setTimeout(() => setAnimation(""), 500);
-  };
 
   const toggleDescription = () => {
     setShowDescription(!showDescription);
@@ -31,7 +21,7 @@ export default function RightPanel() {
 
   return (
     <div
-      className={`flex w-[860px] h-[720px] bg-white rounded-xl p-6 transition-transform duration-500 ${animation} overflow-hidden`}
+      className={`flex relative w-[860px] h-[720px] bg-white rounded-xl p-6 transition-transform duration-500 ${animation} overflow-hidden`}
     >
       {/* Imagen */}
       <div 
@@ -52,24 +42,8 @@ export default function RightPanel() {
             alt={profileData.name}
           />         
 
-          {/* Gradiente oscuro inferior */}
-          <div className="absolute w-full h-full inset-0 rounded-lg bg-[linear-gradient(to_bottom,transparent_0%,transparent_54%,rgba(0,0,0,0.9)_100%)]"></div>
 
-          <div className="absolute pb-2 bottom-7 left-1/2 -translate-x-1/2 flex gap-10">
-            <button
-              onClick={animateLeft}
-              className="bg-white p-6 rounded-full shadow hover:scale-110 transition-transform"
-            >
-              <XMarkIcon className="w-12 h-12 text-[#1A2E53] stroke-[4]" />
-            </button>
 
-            <button
-              onClick={animateRight}
-              className="bg-white p-6 rounded-full shadow hover:scale-110 transition-transform"
-            >
-              <HeartIcon className="w-12 h-12 text-[#FF8269]" />
-            </button>
-          </div>
         </div>
       </div>
 
